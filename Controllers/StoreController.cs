88e685185c9fcf45f4de84f79cb52477f828b98b
Timxthy <Timxthy72@gmail.com>
2022-01-20 12:20:07 +0000

    Polished site navigation

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -36,5 +36,49 @@ namespace WebStore.Controllers
             var phone = _context.Phones.Find(id);
             return View(phone);
         }
+
+        public ActionResult Apple(string brand)
+        {
+            //Displays Apple Brand
+            var phones = _context.Phones.Include("Brand")
+                .Where(c => c.Brand.Name == "Apple");
+            return View(phones);
+        }
+
+        public ActionResult Samsung(string brand)
+        {
+            //Displays Apple Brand
+            var phones = _context.Phones.Include("Brand")
+                .Where(c => c.Brand.Name == "Samsung");
+            return View(phones);
+        }
+
+
+
+        public ActionResult Huawei(string brand)
+        {
+            //Displays Apple Brand
+            var phones = _context.Phones.Include("Brand")
+                .Where(c => c.Brand.Name == "Huawei");
+            return View(phones);
+        }
+
+        public ActionResult Enchro(string brand)
+        {
+            //Displays Apple Brand
+            var phones = _context.Phones.Include("Brand")
+                .Where(c => c.Brand.Name == "Enchro");
+            return View(phones);
+        }
+
+        public ActionResult Google(string brand)
+        {
+            //Displays Apple Brand
+            var phones = _context.Phones.Include("Brand")
+                .Where(c => c.Brand.Name == "Google");
+            return View(phones);
+        }
+
+
     }
 }
